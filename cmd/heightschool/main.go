0ec9b95d6e836edfcb8007e5997b57230e25c575
pankai <kaipan2.a2@gmail.com>
2017-05-13 16:34:05 +0800

    fix

--- cmd/heightschool/main.go
+++ cmd/heightschool/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+
+	"github.com/tealeg/xlsx"
 )
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	excelFileName := "/status/predict_num_v2/constrain/文一批.xlsx"
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		fmt.Printf("error: %s", err)
+	}
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				text, _ := cell.String()
+				fmt.Printf("%s\n", text)
+			}
+		}
+	}
 }
-
