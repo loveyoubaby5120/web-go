2363759da09d684e6093508f1b9dc7d4bc5666ca
pankai <kaipan2.a2@gmail.com>
2017-05-16 13:23:49 +0800

    fix

--- cmd/heightschool/main.go
+++ cmd/heightschool/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"web-go/util/jsonutil"
-	"web-go/util/map2json2map"
 )
 
 type Condition struct {
@@ -13,44 +13,65 @@ type Condition struct {
 	rank        int
 	major       string
 	enrollScore []int
+	isSuccess   bool
 }
 
+var (
+	jsonArray      []Condition
+	scoreCondition []map[string]interface{}
+	scoreArray     []int
+)
+
 func main() {
-	// scoreArray, err := jsonutil.ReadFileArray("static/predict_num_v2/constrain_json/文一批_一分一段.json")
-	// if err != nil {
-	// 	fmt.Println("readFile: ", err.Error())
-	// 	// return nil, err
-	// }
-	scoreCondition, err := jsonutil.ReadFileJsonArray("static/predict_num_v2/constrain_json/文一批_高考分数.json")
+	var err error
+	scoreArray, err := jsonutil.ReadFileArray("static/predict_num_v2/constrain_json/文一批_一分一段.json")
 	if err != nil {
 		fmt.Println("readFile: ", err.Error())
-		// return nil, err
+		return
 	}
+	fmt.Println("获取全部分数成功: ", len(scoreArray))
 
-	// jsonArray := []Condition{}
+	scoreCondition, err = jsonutil.ReadFileJsonArray("static/predict_num_v2/constrain_json/文一批_高考分数.json")
+	if err != nil {
+		fmt.Println("readFile: ", err.Error())
+		return
+	}
 
 	for _, value := range scoreCondition {
-		fmt.Println(map2json2map.Map2json(value))
-		// fmt.Println(value)
-
-		// var jsonC Condition
-		// jsonC.enrollNum = int(value["enroll_num"].(float64))
-		// jsonC.avgScore = int(value["avg_score"].(float64))
-		// jsonC.lowScore = int(value["low_score"].(float64))
-		// jsonC.rank = int(value["rank"].(float64))
-		// jsonC.major = value["major"].(string)
-
-		// var enrollScore []int
-		// for i := 0; i < jsonC.enrollNum; i++ {
-		// 	enrollScore = append(enrollScore, 0)
+		// jsonC, err := map2json2map.Map2json(value)
+		// if err != nil {
+		// 	fmt.Println(err)
 		// }
+		// fmt.Println(string(jsonC))
+		// fmt.Println(value)
+		enrollNum := int(value["enroll_num"].(float64))
+		var enrollScore []int
+		for i := 0; i < enrollNum; i++ {
+			enrollScore = append(enrollScore, 0)
+		}
 
-		// jsonC.enrollScore = enrollScore
-		// fmt.Println(jsonC)
-		// jsonArray := append(jsonArray, jsonC)
-		// fmt.Println(len(jsonArray[0].enrollScore))
+		jsonC := &Condition{
+			enrollNum:   enrollNum,
+			avgScore:    int(value["avg_score"].(float64)),
+			lowScore:    int(value["low_score"].(float64)),
+			rank:        int(value["rank"].(float64)),
+			major:       value["major"].(string),
+			enrollScore: enrollScore,
+		}
+
+		jsonArray = append(jsonArray, *jsonC)
+
 		break
 	}
+
+	fmt.Println("获取配置成功: ", len(jsonArray))
+	outJsonArray, err := json.Marshal(jsonArray)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(jsonArray)
+	jsonutil.WriteJson("static/predict_num_v2/文一批.json", outJsonArray)
+
 }
 
 func Sum(enrollScore []int) int {
